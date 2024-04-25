bad5b3d6eefd738a31ec5e35a7d78d8ee066d927
唐鹏飞 <243813208@qq.com>
2024-04-25 16:37:46 +0800

    增加 springboot集成

---
+++ src/main/java/com/mrt/App.java
@@ -0,0 +1,18 @@
+package com.mrt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Mr.T
+ * @Date: 2024/4/25
+ */
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
